6b3497663fa7553d00212640438902a7d5122ff9
kmitsuishi-08 <61333839+kmitsuishi-08@users.noreply.github.com>
2021-05-04 01:26:43 -0300

    Add dependencies, and API Rest for setup

--- progamer/src/main/java/br/com/fiap/dao/SetupDAO.java
+++ progamer/src/main/java/br/com/fiap/dao/SetupDAO.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import javax.ws.rs.core.Response;
 
 import br.com.fiap.model.Setup;
 import br.com.fiap.util.EntityManagerFactorySingleton;
@@ -11,19 +12,28 @@ import br.com.fiap.util.EntityManagerFactorySingleton;
 public class SetupDAO {
 
 	private EntityManager manager = EntityManagerFactorySingleton.getInstance().createEntityManager();
-	
-	public void save(Setup setup) {
 
+	public void save(Setup setup) {
 		manager.getTransaction().begin();
 		manager.persist(setup);
 		manager.getTransaction().commit();
-		
 		manager.close();
 	}
 
-	public List<Setup>  getAll() {
-		String jpql  = "SELECT s FROM Setup s";
+	public List<Setup> getAll() {
+		String jpql = "SELECT s FROM Setup s";
 		TypedQuery<Setup> createQuery = manager.createQuery(jpql, Setup.class);
 		return createQuery.getResultList();
+	}
+
+	public Setup findById(Long id) {
+		return manager.find(Setup.class, id);
+	}
+
+	public void update(Setup setup) {
+		manager.getTransaction().begin();
+		manager.merge(setup);
+		manager.flush();
+		manager.getTransaction().commit();
 	}
 }
--- progamer/src/main/java/br/com/fiap/model/Setup.java
+++ progamer/src/main/java/br/com/fiap/model/Setup.java
@@ -28,6 +28,10 @@ public class Setup {
 		return id;
 	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
